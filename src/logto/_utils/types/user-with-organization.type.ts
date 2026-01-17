f2aa5ddd548bace9a158b1727c355911e2af9172
gabrielmoraisdev4 <gabrielmoraisdev4@gmail.com>
2026-01-17 12:34:11 +0100

    :recycle: Remove LogtoUserWithoutOrganization type to streamline user organization handling

--- src/logto/_utils/types/user-with-organization.type.ts
+++ src/logto/_utils/types/user-with-organization.type.ts
@@ -1,11 +1,6 @@
 import { LogtoUserClaimOrganization } from '../schemas/logto-payload.types';
 import { LogtoUser, LogtoOrganization } from './responses/responses.type';
 
-export type LogtoUserWithoutOrganization = LogtoUser & {
-  organizations: LogtoUserClaimOrganization[];
-  currentOrganization: null;
-};
-
 export type LogtoUserWithOrganizations = LogtoUser & {
   organizations: LogtoUserClaimOrganization[];
   currentOrganization: LogtoOrganization;
