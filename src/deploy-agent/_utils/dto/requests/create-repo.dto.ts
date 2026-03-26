1627a95c874bc6da72d26ca21b13fd20fc45322f
gabrielmoraisdev4 <gabrielmoraisdev4@gmail.com>
2026-03-26 18:06:43 +0100

    :sparkles: add regex validation for repository name in CreateRepoDto

--- src/deploy-agent/_utils/dto/requests/create-repo.dto.ts
+++ src/deploy-agent/_utils/dto/requests/create-repo.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, MinLength } from 'class-validator';
+import { IsString, Matches, MinLength } from 'class-validator';
+
+export const REPO_NAME_PATTERN = /^[a-z0-9-]+$/;
 
 export class CreateRepoDto {
   @ApiProperty({ example: 'my-repo' })
   @IsString()
   @MinLength(1)
+  @Matches(REPO_NAME_PATTERN)
   name: string;
 }
